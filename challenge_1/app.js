3045e63c1179563c83b3e83be18972b063fefcfa
Faris Habib <faris.habib.7@gmail.com>
2018-11-22 10:28:25 -0800

    Makin my own board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -9,7 +9,40 @@ Minimum Requirements
   -For the initial version of this game, you may only use native DOM methods to listen for events and update the DOM as gameplay progresses.
     This means: no jQuery, React or any other libraries or frameworks. All your code should live in one file, app.js, and all your HTML should live in index.html.
 */
-var chessBoard = function(length) {
+// document.getElementById("x").addEventListener('click', function(event) {
+//   console.log('You clicked x!');
+// });
 
+//Design ascii square, and repeat that square 9 times to create the tic-tac-toe board.
+  //In each square, insert a div tag so we can add click event handlers later
+
+// Default, 3 x 3 board, later you can design more complicated boards
+var makeSquarez = function(row) {
+  makeRow(row);
+  makeColumn(8);
+  document.getElementById("test").appendChild(document.createElement('br'));
+  makeRow(row);
+  // let rowz = document.createTextNode(accRow.join(''));
+  // let columnz = document.createTextNode(accColumn.join('\n'));
+  // let element = document.getElementById("test");
+  // element.appendChild(rowz);
+  // element.appendChild(columnz);
 };
+
+var makeRow = function(num) {
+  for (var i = 0; i <= num; i++) {
+    let asciiz = document.createTextNode('#');
+    document.getElementById("test").appendChild(asciiz);
+  }
+};
+
+var makeColumn = function(num) {
+  for (var i = 0; i <= num; i++) {
+    let asciiz = document.createTextNode('#~~~~~~~~~~~~~~~~#');
+    document.getElementById("test").appendChild(document.createElement('br'));
+    document.getElementById("test").appendChild(asciiz);
+  }
+};
+
+makeSquarez(18);
 
