adc7517b755ddf1a8bf33577ea4cc42aee46d258
Faris Habib <faris.habib.7@gmail.com>
2018-11-22 08:39:34 -0800

    Added board, pieces, and reset button

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,15 @@
-console.log('Hello World');
+/*
+Minimum Requirements
+  -the first move always starts with player X
+  -the app detects a win or tie and displays an appropriate message
+  -a button resets the game for a new round of gameplay
+  -It's a single page app, meaning once the page loads, no user-generated actions on the page may cause the entire page to reload.
+    - One way this rule could be broken in your implementation is that you might consider resetting the game state by refreshing the page.
+      This technique is not permitted for this exercise.
+  -For the initial version of this game, you may only use native DOM methods to listen for events and update the DOM as gameplay progresses.
+    This means: no jQuery, React or any other libraries or frameworks. All your code should live in one file, app.js, and all your HTML should live in index.html.
+*/
+var chessBoard = function(length) {
+
+};
 
