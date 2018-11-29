311a56f8f2b7c16cb16e2939777cc33f8edf185a
Faris Habib <faris.habib.7@gmail.com>
2018-11-28 19:49:45 -0800

    Completed form 1

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+class PersonalInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
 
-const name = 'Faris Habib';
-const element = <h1>Hello {name}</h1>;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-ReactDOM.render(element, document.getEelementById('root'));
+  handleChange(event) {
+    this.setState({
+      [event.target.name] : event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('Name is: ' + this.state.name + '\nemail is: ' + this.state.email + '\npassword is ' + this.state.password);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input name="email" type="text" value={this.state.email} onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input name="password" type="text" value={this.state.password} onChange={this.handleChange} />
+        </label>
+        <br />
+        <input type="submit" value="Next" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<PersonalInfo/>, document.getElementById('form1'));
