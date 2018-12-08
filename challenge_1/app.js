9777c0464d9a6c09f879ea00dddd519805d1fc74
Faris Habib <faris.habib.7@gmail.com>
2018-12-07 21:21:36 -0800

    Complete challenge 1 and partially complete challenge 3

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,62 +1,147 @@
-/*
-Minimum Requirements
-  -the first move always starts with player X
-  -the app detects a win or tie and displays an appropriate message
-  -a button resets the game for a new round of gameplay
-  -It's a single page app, meaning once the page loads, no user-generated actions on the page may cause the entire page to reload.
-    - One way this rule could be broken in your implementation is that you might consider resetting the game state by refreshing the page.
-      This technique is not permitted for this exercise.
-  -For the initial version of this game, you may only use native DOM methods to listen for events and update the DOM as gameplay progresses.
-    This means: no jQuery, React or any other libraries or frameworks. All your code should live in one file, app.js, and all your HTML should live in index.html.
-*/
-// document.getElementById("x").addEventListener('click', function(event) {
-//   console.log('You clicked x!');
-// });
-
-//Design ascii square, and repeat that square 9 times to create the tic-tac-toe board.
-  //In each square, insert a div tag so we can add click event handlers later
-
-// Default, 3 x 3 board, later you can design more complicated boards
-// var makeSquarez = function(id) {
-//   makeBorderz(id);
-//   makeGridz(id);
-//   document.getElementById(id).appendChild(document.createElement('br'));
-//   makeBorderz(id);
-//   // let rowz = document.createTextNode(accRow.join(''));
-//   // let columnz = document.createTextNode(accColumn.join('\n'));
-//   // let element = document.getElementById("test");
-//   // element.appendChild(rowz);
-//   // element.appendChild(columnz);
-// };
-
-// var makeBorderz = function(id) {
-//   for (var i = 0; i <= 18; i++) {
-//     let asciiz = document.createTextNode('#');
-//     document.getElementById(id).appendChild(asciiz);
-//   }
-// };
-
-// var makeGridz = function(id) {
-//   for (var i = 0; i <= 8; i++) {
-//     let asciiz = document.createTextNode('#~~~~~~~~~~~~~~~~#');
-//     document.getElementById(id).appendChild(document.createElement('br'));
-//     document.getElementById(id).appendChild(asciiz);
-//   }
-// };
-
-// makeSquarez('A1');
-// makeSquarez('B1');
-
-document.getElementById('A1').addEventListener('click', function(event) {
-  console.log('You clicked here!');
-  let xPiece = document.createElement('img');
-  xPiece.src = 'tic-tac-toe-X.png';
-  xPiece.setAttribute('id', 'x');
-  xPiece.setAttribute('alt', 'tic-tac-toe-X');
-  xPiece.setAttribute('width', '100');
-  xPiece.setAttribute('height', '100');
-  document.getElementById('A1').appendChild(xPiece);
-  //debugger;
-});
+const boardState = {
+  active: 'X',
+  turnCounter: 0,
+  O: {
+    className: 'playerO',
+    classNames: {}
+  },
+  X: {
+    className: 'playerX',
+    classNames: {}
+  }
+};
 
 
+const turnDisplayHighlight = (className) => {
+  let currentPlayer = document.getElementsByClassName(className);
+  currentPlayer[0].setAttribute(
+    'style',
+    'color: orange; display: inline;'
+  );
+};
+
+const turnDisplayCancel = (className) => {
+  let currentPlayer = document.getElementsByClassName(className);
+  currentPlayer[0].setAttribute('style', 'display: inline; margin: 5em;');
+};
+
+const changePlayerTurn = (currentPlayer) => {
+  if (currentPlayer === 'X') {
+    boardState.active = 'O';
+  } else if (currentPlayer === 'O') {
+    boardState.active = 'X';
+  }
+  const nextPlayer = boardState.active;
+  turnDisplayHighlight(boardState[nextPlayer].className);
+  turnDisplayCancel(boardState[currentPlayer].className);
+  changeMessage(nextPlayer);
+};
+
+const changeMessage = (string) => {
+  const message = document.getElementsByClassName('messages');
+  if (string.length === 1) {
+    message[0].innerHTML = `Player ${string}'s turn`;
+  } else if (string === 'Piece Exists') {
+    message[0].innerHTML = `A piece already exists on this spot.`;
+  } else {
+    message[0].innerHTML = string;
+  }
+};
+
+const placePiece = (eventObj) => {
+  const boardTileSelector = eventObj.target.className;
+  const boardTile = document.getElementsByClassName(boardTileSelector);
+  const openTile = boardTile[0].innerHTML.length === 0;
+  if (openTile) {
+    boardState.turnCounter++;
+    const piece = boardState.active;
+    boardTile[0].innerHTML = piece;
+    manageState(boardTile[0].classList, piece);
+    changePlayerTurn(piece);
+    winCalculator();
+  } else {
+    changeMessage('Piece Exists');
+  }
+};
+
+const manageState = (array, piece) => {
+  array.forEach(className => {
+    const obj = boardState[piece].classNames;
+    if (obj[className]) {
+      obj[className]++;
+    } else {
+      obj[className] = 1;
+    }
+  });
+};
+
+const winCalculator = () => {
+  let hasWinner = false;
+  for (let key in boardState) {
+    for (let position in boardState[key].classNames) {
+      if (boardState[key].classNames[position] === 3) {
+        hasAWinner = true;
+        handleWinner(key);
+      }
+    }
+  }
+  if (!hasWinner && boardState.turnCounter === 9) {
+    handleWinner('Tie');
+  }
+};
+
+const handleWinner = (winner) => {
+  turnDisplayCancel(boardState.O.className);
+  turnDisplayCancel(boardState.X.className);
+
+  const board = document.getElementById('board');
+  board.removeEventListener('click', placePiece);
+
+  if (winner === 'Tie') {
+    changeMessage('Tie Game!');
+  } else {
+    changeMessage(`Player ${winner} wins!`);
+  }
+
+  createButton();
+};
+
+const refreshBoard = () => {
+  let tiles = document.getElementsByTagName('td');
+  Array.prototype.forEach.call(tiles, boardTile => {
+    if (boardTile.innerHTML.length) {
+      boardTile.innerHTML = '';
+    }
+  });
+  init();
+};
+
+const createButton = () => {
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Restart Game';
+  const refreshBtn = document.getElementsByClassName('refresh')[0];
+  refreshBtn.appendChild(btn);
+
+  const refresh = document.getElementById('refresh');
+  refresh.addEventListener('click', refreshBoard);
+};
+
+
+const init = () => {
+  const refreshBtn = document.getElementsByTagName('button')[0];
+  if (refreshBtn) {
+    refreshBtn.remove();
+  }
+
+  boardState.turnCounter = 0;
+  boardState.active = 'X';
+  boardState.X.classNames = {};
+  boardState.O.classNames = {};
+
+  const board = document.getElementById('board');
+  board.addEventListener('click', placePiece);
+  turnDisplayHighlight(boardState.X.className);
+  changeMessage(boardState.active);
+};
+
+init();
